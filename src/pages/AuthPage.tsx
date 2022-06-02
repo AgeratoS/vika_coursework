2035b558df663072230cb5dbc9e97f4e13b0b97f
thetiredsun <leshka99514@gmail.com>
2022-06-02 17:30:24 +0300

    Fixed problem with domain

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -3,7 +3,7 @@ import React, {useEffect} from "react";
 const AuthPage: React.FC = () => {
 
     useEffect(() => {
-        window.location.href = 'https://626fc8e39dd8360009c14a23.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=75rnslps1qtkeejbtod8hhisp0&response_type=token&state=STATE&redirect_uri=http://localhost:3000/auth'
+        window.location.href = 'https://626fc8e39dd8360009c14a23.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=75rnslps1qtkeejbtod8hhisp0&response_type=token&state=STATE&redirect_uri=https://vika-coursework.vercel.app/auth'
     }, []);
 
     return (
--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect} from "react";
-import {useNavigate} from "react-router-dom";
 
 const LogoutPage: React.FC = () => {
 
     useEffect(() => {
         setTimeout(() => {
-            window.location.href = 'https://626fc8e39dd8360009c14a23.auth.us-east-1.amazoncognito.com/logout?client_id=75rnslps1qtkeejbtod8hhisp0&redirect_uri=http://localhost:3000&response_type=token&state=STATE';
+            window.location.href = 'https://626fc8e39dd8360009c14a23.auth.us-east-1.amazoncognito.com/logout?client_id=75rnslps1qtkeejbtod8hhisp0&redirect_uri=https://vika-coursework.vercel.app&response_type=token&state=STATE';
         }, 2000);
     }, []);
 
